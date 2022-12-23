4cda3b2d1c701f6c31d7efacef6a393b4fc0316b
Anatoliy Shulika <shulika.anatoliy2@gmail.com>
2022-12-23 18:23:46 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,67 @@
+import { Component } from 'react';
+import axios from 'axios';
+import ContentLoader from 'react-content-loader';
+import fetchArticlesWithQuery from 'services/api';
+
+axios.defaults.baseURL = 'https://hn.algolia.com/api/v1';
+const MyLoader = () => (
+  <ContentLoader
+    height={140}
+    speed={1}
+    backgroundColor={'#333'}
+    foregroundColor={'#999'}
+    viewBox="0 0 380 70"
+  >
+    {/* Only SVG shapes */}
+    <rect x="0" y="0" rx="5" ry="5" width="70" height="70" />
+    <rect x="80" y="17" rx="4" ry="4" width="300" height="13" />
+    <rect x="80" y="40" rx="3" ry="3" width="250" height="10" />
+  </ContentLoader>
+);
+
+const ArticleList = ({ articles }) => (
+  <ul>
+    {articles.map(({ objectID, url, title }) => (
+      <li key={objectID}>
+        <a href={url} target="_blank" rel="noreferrer noopener">
+          {title}
+        </a>
+      </li>
+    ))}
+  </ul>
+);
+
+class App extends Component {
+  state = {
+    articles: [],
+    isLoading: false,
+    error: null,
+  };
+
+  async componentDidMount() {
+    this.setState({ isLoading: true });
+
+    try {
+      const articles = await fetchArticlesWithQuery('react');
+      this.setState({ articles });
+    } catch (error) {
+      this.setState({ error });
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  }
+
+  render() {
+    const { articles, isLoading, error } = this.state;
+
+    return (
+      <div>
+        {error && <p>Whoops, something went wrong: {error.message}</p>}
+        {isLoading && <MyLoader />}
+        {articles.length > 0 && <ArticleList articles={articles} />}
+      </div>
+    );
+  }
+}
+
+export default App;
